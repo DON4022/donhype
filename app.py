e9be0a1f3a8231330cf00b19ed642368161de272
ROBERT KIPRONO <kipronorobert529@gmail.com>
2026-05-28 16:16:26 +0300

    Integrate Streamlit to display HTML content

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.set_page_config(layout="wide")
+
+# This opens your HTML file and reads it
+with open("site.html", "r", encoding="utf-8") as file:
+    html_content = file.read()
+
+# This displays it on the screen
+st.markdown(html_content, unsafe_allow_html=True)
